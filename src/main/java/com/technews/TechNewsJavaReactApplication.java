a30576bc68c6c1595b124b132285595ab4cbe99b
ctnohilly <ctnohilly@gmail.com>
2023-03-15 17:03:27 -0400

    Initial project commit with backend API and initial React app

---
+++ src/main/java/com/technews/TechNewsJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaReactApplication.class, args);
+	}
+
+}
